65afe4221a9c696791de87349d6cbbcd309d22b0
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-05-24 11:06:42 +0900

    add ogp

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,9 +1,27 @@
 import { Link, Meta, MetaProvider, Title } from "@solidjs/meta";
+import { useLanguage } from "./util/useLanguage";
 
 export default function Head() {
+  const isJapanese = useLanguage();
+
   return (
     <MetaProvider>
       <Title>Luminostr</Title>
+      <Link rel="icon" href="./favicon.svg" />
+      <Meta property="og:type" content="website" />
+      <Meta property="og:title" content="Luminostr" />
+      <Meta
+        property="og:image"
+        content={`${window.location.origin}/favicon.svg`}
+      />
+      <Meta
+        property="og:description"
+        content={`${
+          isJapanese()
+            ? "置換可能（replaceable）イベントのリカバリーツール"
+            : "Replacementable Event Recovery Tool"
+        }`}
+      />
     </MetaProvider>
   );
 }
